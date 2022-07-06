fd37a052876eaaf91c677b9ba7a0ee7796abff42
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-06 15:35:12 -0500

    incorporate changes into main

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var stockEod
 var stockRtd
 var numOfDays = 14
 
-
 // =============== Fetch Functions ===============
 const fetchStockEODHistorical = async (companySymbols) => {
     let stockEODHistoricalresponse = await fetch (`https://api.stockdata.org/v1/data/eod?symbols=${companySymbols}&api_token=${stockAPIKey}`)
@@ -21,7 +20,6 @@ const fetchStockEODHistorical = async (companySymbols) => {
     }
 };
 
-
 const fetchStockRealTime = async (companySymbols) => {
     let stockRealTimeresponse = await fetch (`https://api.stockdata.org/v1/data/quote?symbols=${companySymbols}&api_token=${stockAPIKey}`)
     let realTimeData = await stockRealTimeresponse.json();
@@ -37,4 +35,47 @@ const fetchStockRealTime = async (companySymbols) => {
 
 
 fetchStockEODHistorical("AAPL")
-//fetchStockRealTime("AAPL")
+fetchStockRealTime("AAPL")
+
+/**
+ * add event listener for search button and get data for newspaper on submit
+ */
+var newsApiKey = "9PncQC7G9Fw1IBbcYpjiZa1T4of4Qrgq";
+var formEl = $("#search_form");
+var searchInputEl = $("#search_input");
+formEl.on("submit", function (e) {
+  e.preventDefault();
+  console.log(searchInputEl.val());
+  var inputVal = searchInputEl.val();
+
+  //getTicker()
+
+  getTicker(inputVal);
+  // convert search input into company proper name 'Apple or AAPL' -> 'Apple Inc.'Apple
+  //   getNewsData(inputVal);
+});
+const getTicker = async (input) => {
+  const response = await fetch(
+    `https://yfapi.net/v6/finance/autocomplete?region=US&lang=en&query=${input}`,
+    {
+      headers: {
+        "x-api-key": " AGCJTVhXEI6nit286CVCQ9ArKw62Ejwxapo8eKgW",
+      },
+    }
+  );
+  const data = await response.json();
+
+  console.log(data.ResultSet.Result[0]);
+};
+const getNewsData = async (input) => {
+  console.log(input);
+  let newsDataResponse = await fetch(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${input}&api-key=${newsApiKey}`
+  );
+  let newsData = await newsDataResponse.json();
+  console.log(newsData.response.docs);
+};
+function init() {}
+init();
+
+// console.log(newdata());
