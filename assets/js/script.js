d87b2a341d367c9ddf9861b1188f0e9af48249a6
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-06 14:20:18 -0500

    log out real time data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
-stockAPIKey = "K6okZSBQ1g8zI1JkQgobaOIGzVbCvq3aSNcaARG0"
+// =============== Variables ===============
+var stockAPIKey = "K6okZSBQ1g8zI1JkQgobaOIGzVbCvq3aSNcaARG0"
+var stockEod
+var stockRtd
 
 const fetchStockEODHistorical = async (companySymbols) => {
     let stockEODHistoricalresponse = await fetch (`https://api.stockdata.org/v1/data/eod?symbols=${companySymbols}&api_token=${stockAPIKey}`)
     console.log (stockEODHistoricalresponse)
     let eodData = await stockEODHistoricalresponse.json();
-    console.log (eodData)
+    stockEod = eodData
+    console.log (stockEod);
 };
 
 
 const fetchStockRealTime = async (companySymbols) => {
     let stockRealTimeresponse = await fetch (`https://api.stockdata.org/v1/data/quote?symbols=${companySymbols}&api_token=${stockAPIKey}`)
     console.log (stockRealTimeresponse)
-    let eodData = await stockRealTimeresponse.json();
-    console.log (eodData)
+    let realTimeData = await stockRealTimeresponse.json();
+    stockRtd = realTimeData
+    console.log (stockRtd)
+    console.log("52 week high:", stockRtd.data[0]["52_week_high"]);
+    console.log("52 week low:", stockRtd.data[0]["52_week_low"]);
+    console.log("day high:", stockRtd.data[0].day_high);
+    console.log("day low:", stockRtd.data[0].day_low);
+    console.log("day open", stockRtd.data[0].day_open);
+    console.log("price:", stockRtd.data[0].price);
 };
 
 
 fetchStockEODHistorical("AAPL")
-console.log('%c ==================== SPACER ====================', 'color:orange;font-weight:900');
-fetchstockRealTime("AAPL")
+fetchStockRealTime("AAPL")
