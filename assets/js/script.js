e0152e7a06de310ef477ee7581f3a420eb916418
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-06 15:50:53 -0500

    delete redundant functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ const fetchStockEODHistorical = async (companySymbols) => {
     let stockEODHistoricalresponse = await fetch (`https://api.stockdata.org/v1/data/eod?symbols=${companySymbols}&api_token=${stockAPIKey}`)
     let eodData = await stockEODHistoricalresponse.json();
     stockEod = eodData
-    console.log (stockEod);
     for (var i = 0; i < numOfDays ; i++) {
         console.log ("date", stockEod.data[i].date);        
         console.log ("close", stockEod.data[i].close);
@@ -41,62 +40,6 @@ const fetchStockRealTime = async (companySymbols) => {
   let eodData = await stockEODHistoricalresponse.json();
   console.log(eodData);
 };
-
-const fetchStockRealTime = async (companySymbols) => {
-  let stockRealTimeresponse = await fetch(
-    `https://api.stockdata.org/v1/data/quote?symbols=${companySymbols}&api_token=${stockAPIKey}`
-  );
-  console.log(stockRealTimeresponse);
-  let eodData = await stockRealTimeresponse.json();
-  console.log(eodData);
-
-};
-
-fetchStockEODHistorical("AAPL");
-
-fetchStockRealTime("AAPL");
-
-/**
- * add event listener for search button and get data for newspaper on submit
- */
-var newsApiKey = "9PncQC7G9Fw1IBbcYpjiZa1T4of4Qrgq";
-var formEl = $("#search_form");
-var searchInputEl = $("#search_input");
-formEl.on("submit", function (e) {
-  e.preventDefault();
-  console.log(searchInputEl.val());
-  var inputVal = searchInputEl.val();
-
-  //getTicker()
-
-  getTicker(inputVal);
-  // convert search input into company proper name 'Apple or AAPL' -> 'Apple Inc.'Apple
-  //   getNewsData(inputVal);
-});
-const getTicker = async (input) => {
-  const response = await fetch(
-    `https://yfapi.net/v6/finance/autocomplete?region=US&lang=en&query=${input}`,
-    {
-      headers: {
-        "x-api-key": " AGCJTVhXEI6nit286CVCQ9ArKw62Ejwxapo8eKgW",
-      },
-    }
-  );
-  const data = await response.json();
-
-  console.log(data.ResultSet.Result[0]);
-};
-const getNewsData = async (input) => {
-  console.log(input);
-  let newsDataResponse = await fetch(
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${input}&api-key=${newsApiKey}`
-  );
-  let newsData = await newsDataResponse.json();
-  console.log(newsData.response.docs);
-};
-function init() {}
-init();
-
 
 fetchStockEODHistorical("AAPL")
 fetchStockRealTime("AAPL")
